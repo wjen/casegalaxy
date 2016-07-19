34c0af3e9687fed2fa93f065d7b67847eaaf9a3f
Wen Jen <wjen85@gmail.com>
2016-07-19 02:17:08 -0700

    update gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 //uglify is used only for js not css
 var uglify = require('gulp-uglify');
+
+
+gulp.task('firstTask', function() {
+  console.log('Hello');
+});
