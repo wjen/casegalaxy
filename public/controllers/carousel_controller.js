7cfa8a5070ed78184de818e4258030e0c6c1f22c
Wen Jen <wjen85@gmail.com>
2016-07-22 10:51:37 -0700

    fix error

--- public/controllers/carousel_controller.js
+++ public/controllers/carousel_controller.js
@@ -13,17 +13,17 @@
     vm.active = 0;
     var slides = vm.slides = [
       {
-        image: "http://i.imgur.com/0oIek4C.jpg?1%3E%3C/div%3E%3Cdiv%20style=",
+        image: "//i.imgur.com/0oIek4C.jpg?1%3E%3C/div%3E%3Cdiv%20style=",
         title: "HYBRID CASES",
         text: "Dual-Layer Protection",
       },
       {
-        image: "http://store.imore.com/images/product_images/accessories/additional_images/18500/large/1.jpg",
+        image: "//store.imore.com/images/product_images/accessories/additional_images/18500/large/1.jpg",
         title: "TPU BUMPER CASES",
         text: "Protection With Style",
       },
       {
-        image: "http://www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2015/03/galaxy-s6-spigen-wallet-case-brown-2QL.jpg?itok=MDJQKjhK",
+        image: "//www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2015/03/galaxy-s6-spigen-wallet-case-brown-2QL.jpg?itok=MDJQKjhK",
         title: "PREMIUM WALLET CASES",
         text: "Protection With Convenience",
       }
