0ec62d76833a3f628a9dd718ff9b78b598bdfee8
Wen Jen <wjen85@gmail.com>
2016-09-19 11:40:53 -0700

    add links to carousel images

--- public/controllers/carousel_controller.js
+++ public/controllers/carousel_controller.js
@@ -16,16 +16,19 @@
         image: "//i.imgur.com/0oIek4C.jpg?1%3E%3C/div%3E%3Cdiv%20style=",
         title: "HYBRID CASES",
         text: "Dual-Layer Protection",
+        sref: "itemsPage"
       },
       {
         image: "https://appsmartmall.com/wp-content/uploads/2015/12/iPhone-6-Case-Hotbin-Protective-Rubber-iPhone-6-Case-Crystal-Clear-Flexible-Soft-TPU-Case-Ultra-Slim-Case-for-iPhone-6-Clear-0-0.jpg",
         title: "TPU BUMPER CASES",
         text: "Protection With Style",
+        sref: "itemsPage"
       },
       {
         image: "https://masetv.com/wp-content/uploads/2014/09/spigen-wallet-case-iphone-6.jpg",
         title: "PREMIUM WALLET CASES",
         text: "Protection With Convenience",
+        sref: "itemsPage"
       }
     ];
   };
